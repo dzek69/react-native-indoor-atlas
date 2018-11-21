b8e71b7c226710a4c71a0b7cc9e27069e5e65329
dzek69 <git-public@dzek.eu>
2018-11-21 20:46:55 +0100

    - added basic JavaScript code to attach listeners

---
+++ src/NativeEventsInitalizer.js
@@ -0,0 +1,27 @@
+import { NativeModules } from 'react-native';
+
+const { IndoorAtlas } = NativeModules;
+
+class NativeEventsInitializer {
+    constructor() {
+        this._total = 0;
+    }
+
+    increase(count = 1) {
+        this._total += count;
+        if (this._total === 1) {
+            IndoorAtlas.listen();
+        }
+    }
+
+    decrease(count = 1) {
+        this._total -= count;
+        if (this._total === 0) {
+            IndoorAtlas.stop();
+        }
+    }
+}
+
+const instance = NativeEventsInitializer();
+
+export default instance;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,77 @@
-module.exports = () => {};
+import { NativeModules, NativeEventEmitter } from 'react-native';
+
+import eventsInitializer from "./NativeEventsInitalizer";
+
+const { IndoorAtlas } = NativeModules;
+
+const NOT_FOUND = -1;
+
+class IndoorAtlas {
+    constructor() {
+        this._listeners = {
+            locationChanged: [],
+            statusChanged: [],
+        };
+
+        this.on = this.addListener;
+        this.off = this.removeListener;
+
+        const indoorAtlasEventEmitter = new NativeEventEmitter(IndoorAtlas);
+        Object.keys(this._listeners).forEach(eventName => {
+            const propertyName = "_" + eventName + "Subscription";
+            const listener = this._handleIndoorAtlasEvent.bind(this, eventName);
+            this[propertyName] = indoorAtlasEventEmitter.addListener(listener);
+        });
+    }
+
+    _handleIndoorAtlasEvent(eventName, data) {
+        const array = this._listeners[eventName];
+        if (!array) {
+            return;
+        }
+
+        array.forEach(listener => listener(data));
+    }
+
+    addListener(eventName, listener) {
+        const array = this._listeners[eventName];
+        if (!array) {
+            throw new Error("Unknown event");
+        }
+        if (typeof listener !== "function") {
+            throw new TypeError("Listener must be a function");
+        }
+        if (array.includes(listener)) {
+            throw new Error("Listener already attached");
+        }
+
+        array.push(listener);
+        eventsInitializer.increase();
+    }
+
+    removeListener(eventName, listener) {
+        const array = this._listeners[eventName];
+        if (!array) {
+            throw new Error("Unknown event");
+        }
+        const index = array.indexOf(listener);
+        if (index === NOT_FOUND) {
+            throw new Error("Listener wasn't attached or is already removed");
+        }
+
+        array.splice(index, 1);
+        eventsInitializer.decrease();
+    }
+
+    destroy() {
+        Object.entries(this._listeners).forEach(([eventName, eventArray]) => {
+            const subscriberPropertyName = "_" + eventName + "Subscription";
+            this[subscriberPropertyName].remove();
+
+            eventsInitializer.decrease(eventArray.length);
+            eventArray.length = 0;
+        });
+    }
+}
+
+export default IndoorAtlas;
